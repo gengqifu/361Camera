ff9aee40e117609ff6ae52ba94cfae1e5c1b612f
qifu <qifu@jiaju.com>
2017-08-30 18:41:24 +0800

    Switch camera between back and front.

--- 361camera/src/main/java/org/hunter/a361camera/view/CameraFragment.java
+++ 361camera/src/main/java/org/hunter/a361camera/view/CameraFragment.java
@@ -1,6 +1,7 @@
 package org.hunter.a361camera.view;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -49,11 +50,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 /**
  * Main UI for the statistics screen.
  */
 public class CameraFragment extends Fragment {
+    public static final String CAMERA_FRONT = "1";
+    public static final String CAMERA_BACK = "0";
+
     public static final int REQUEST_CAMERA_CODE = 100;
     private static final int REQUEST_EXTERNAL_STORAGE = 101;
     private static String[] PERMISSIONS_STORAGE = {
@@ -87,7 +92,7 @@ public class CameraFragment extends Fragment {
 
     private ImageView ivShow;
     private ImageView mCatture;
-    private Handler childHandler, mainHandler;
+    private ImageView mSwitchCamera;
     private String mCameraId;
     private Size mPreviewSize;
     private Size mCaptureSize;
@@ -99,32 +104,17 @@ public class CameraFragment extends Fragment {
     private CaptureRequest.Builder mCaptureRequestBuilder;
     private CaptureRequest mCaptureRequest;
     private CameraCaptureSession mCameraCaptureSession;
-    CameraManager mCameraManager;
+    private CameraManager mCameraManager;
+    /**
+     * A {@link Semaphore} to prevent the app from exiting before closing the camera.
+     */
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
 
     /**
-     * 摄像头创建监听
+     * A reference to the current {@link android.hardware.camera2.CameraCaptureSession} for
+     * preview.
      */
-    /*private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(CameraDevice camera) {//打开摄像头
-            mCameraDevice = camera;
-            //开启预览
-            takePreview();
-        }
 
-        @Override
-        public void onDisconnected(CameraDevice camera) {//关闭摄像头
-            if (null != mCameraDevice) {
-                mCameraDevice.close();
-                CameraFragment.this.mCameraDevice = null;
-            }
-        }
-
-        @Override
-        public void onError(CameraDevice camera, int error) {//发生错误
-            Toast.makeText(getContext(), "摄像头开启失败", Toast.LENGTH_SHORT).show();
-        }
-    };*/
     public static CameraFragment newInstance() {
         return new CameraFragment();
     }
@@ -451,86 +441,20 @@ public class CameraFragment extends Fragment {
      */
     private void initVIew(View root) {
         ivShow = (ImageView) root.findViewById(R.id.iv_show_camera2);
-        //mSurfaceView
         mTextureView = (TextureView) root.findViewById(R.id.texture_view_camera2);
         mCatture = (ImageView) root.findViewById(R.id.capture);
-        /*mSurfaceHolder = mSurfaceView.getHolder();
-        mSurfaceHolder.setKeepScreenOn(true);
-        // mSurfaceView添加回调
-        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) { //SurfaceView创建
-                // 初始化Camera
-                initCamera2();
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) { //SurfaceView销毁
-                // 释放Camera资源
-                if (null != mCameraDevice) {
-                    mCameraDevice.close();
-                    CameraFragment.this.mCameraDevice = null;
-                }
-            }
-        });*/
+        mSwitchCamera = (ImageView) root.findViewById(R.id.switch_camera);
     }
 
-    /**
-     * 初始化Camera2
-     */
-    /*@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void initCamera2() {
-        HandlerThread handlerThread = new HandlerThread("Camera2");
-        handlerThread.start();
-        childHandler = new Handler(handlerThread.getLooper());
-        mainHandler = new Handler(getMainLooper());
-        mCameraID = "" + CameraCharacteristics.LENS_FACING_FRONT;//后摄像头
-        mImageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1);
-        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() { //可以在这里处理拍照得到的临时照片 例如，写入本地
-            @Override
-            public void onImageAvailable(ImageReader reader) {
-                //mCameraDevice.close();
-                // 拿到拍照照片数据
-                Image image = reader.acquireNextImage();
-                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                byte[] bytes = new byte[buffer.remaining()];
-                buffer.get(bytes);//由缓冲区存入字节数组
-                final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                if (bitmap != null) {
-                    ivShow.setImageBitmap(bitmap);
-                }
-            }
-        }, mainHandler);
-        //获取摄像头管理
-        mCameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                //申请WRITE_EXTERNAL_STORAGE权限
-                requestPermissions(new String[]{Manifest.permission.CAMERA},
-                        REQUEST_CAMERA_CODE);
-                //return;
-            } else {
-                //打开摄像头
-                mCameraManager.openCamera(mCameraID, stateCallback, mainHandler);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }*/
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_CAMERA_CODE:
-                //if (requestCode == REQUEST_CAMERA_CODE) {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // Permission Granted
                     try {
-                        mCameraManager.openCamera(mCameraId, mStateCallback, mainHandler);
+                        mCameraManager.openCamera(mCameraId, mStateCallback, mCameraHandler);
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     } catch (SecurityException e) {
@@ -546,77 +470,9 @@ public class CameraFragment extends Fragment {
                 } else {
 
                 }
-                //}
         }
     }
 
-    /**
-     * 开始预览
-     */
-    /*private void takePreview() {
-        try {
-            // 创建预览需要的CaptureRequest.Builder
-            final CaptureRequest.Builder previewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            // 将SurfaceView的surface作为CaptureRequest.Builder的目标
-            previewRequestBuilder.addTarget(mSurfaceHolder.getSurface());
-            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
-            mCameraDevice.createCaptureSession(Arrays.asList(mSurfaceHolder.getSurface(), mImageReader.getSurface()), new CameraCaptureSession.StateCallback() // ③
-            {
-                @Override
-                public void onConfigured(CameraCaptureSession cameraCaptureSession) {
-                    if (null == mCameraDevice) return;
-                    // 当摄像头已经准备好时，开始显示预览
-                    mCameraCaptureSession = cameraCaptureSession;
-                    try {
-                        // 自动对焦
-                        previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-                        // 打开闪光灯
-                        previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-                        // 显示预览
-                        CaptureRequest previewRequest = previewRequestBuilder.build();
-                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(getContext(), "配置失败", Toast.LENGTH_SHORT).show();
-                }
-            }, childHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    /**
-     * 拍照
-     */
-    /*private void takePicture() {
-        if (mCameraDevice == null) return;
-        // 创建拍照需要的CaptureRequest.Builder
-        final CaptureRequest.Builder captureRequestBuilder;
-        try {
-            captureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            // 将imageReader的surface作为CaptureRequest.Builder的目标
-            captureRequestBuilder.addTarget(mImageReader.getSurface());
-            // 自动对焦
-            captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-            // 自动曝光
-            captureRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-            // 获取手机方向
-            int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
-            // 根据设备方向计算设置照片的方向
-            captureRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
-            //拍照
-            CaptureRequest mCaptureRequest = captureRequestBuilder.build();
-            mCameraCaptureSession.capture(mCaptureRequest, null, childHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-*/
     private void initListener() {
         mCatture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -624,5 +480,87 @@ public class CameraFragment extends Fragment {
                 takePicture();
             }
         });
+
+        mSwitchCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchCamera();
+            }
+        });
+    }
+
+    private void switchCamera() {
+        if (mCameraId.equals(CAMERA_FRONT)) {
+            mCameraId = CAMERA_BACK;
+            closeCamera();
+            reopenCamera();
+        } else if (mCameraId.equals(CAMERA_BACK)) {
+            mCameraId = CAMERA_FRONT;
+            closeCamera();
+            reopenCamera();
+        }
+    }
+
+    /**
+     * Tries to open a {@link CameraDevice}. The result is listened by `mStateCallback`.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void openCamera(int width, int height) {
+        final Activity activity = getActivity();
+        if (null == activity || activity.isFinishing()) {
+            return;
+        }
+        //CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = mCameraManager.getCameraIdList()[0];
+
+            // Choose the sizes for camera preview and video recording
+            CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics
+                    .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            if (map == null) {
+                throw new RuntimeException("Cannot get available preview/video sizes");
+            }
+            mPreviewSize = getOptimalSize(map.getOutputSizes(SurfaceTexture.class),
+                    width, height);
+            mCameraManager.openCamera(cameraId, mStateCallback, null);
+        } catch (CameraAccessException e) {
+            Toast.makeText(activity, "Cannot access the camera.", Toast.LENGTH_SHORT).show();
+            activity.finish();
+        } catch (NullPointerException e) {
+            // Currently an NPE is thrown when the Camera2API is used but not supported on the
+            // device this code runs.
+        }
+    }
+
+    private void closeCamera() {
+        try {
+            mCameraOpenCloseLock.acquire();
+            closePreviewSession();
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                mCameraDevice = null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera closing.");
+        } finally {
+            mCameraOpenCloseLock.release();
+        }
+    }
+
+    public void reopenCamera() {
+        if (mTextureView.isAvailable()) {
+            //openCamera(mTextureView.getWidth(), mTextureView.getHeight());
+            openCamera();
+        } else {
+            mTextureView.setSurfaceTextureListener(mTextureListener);
+        }
+    }
+
+    private void closePreviewSession() {
+        if (mCameraCaptureSession != null) {
+            mCameraCaptureSession.close();
+            mCameraCaptureSession = null;
+        }
     }
 }
